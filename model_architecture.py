8496e97729938c76b399f337bd3a842d6f5ca55d
lmacionis <lmacionis@github.com>
2025-01-30 20:54:54 +0200

    created EurSatCNN module architecture

--- model_architecture.py
+++ model_architecture.py
@@ -40,15 +40,34 @@ print(f"Testavimo rinkinys: {len(test_dataset)} vaizdų")
 class EurSatCNN(nn.Module):
     def __init__(self):
         super(EurSatCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 2, 2, stride=1, padding=1, padding_mode="reflect")   # RGB 3 Channels, feature countint according to formula, kernel / filter priklauso nuo paveiksliuko dydžio jei mažas imam maža kernel, jei didelis t.y. dideli.
+        self.conv1 = nn.Conv2d(3, 4, 3, stride=1, padding=1, padding_mode="reflect")   # RGB 3 Channels, feature counting according to formula, kernel / filter priklauso nuo paveiksliuko dydžio jei mažas imam maža kernel, jei didelis t.y. dideli.
         self.dropout1 = nn.Dropout(0.2)     # Can increace this according if its underfitting or overfitting
-        self.conv2 = nn.Conv2d(2, 4, 2, stride=1, padding=1, padding_mode="reflect")
+        self.conv2 = nn.Conv2d(4, 8, 3, stride=1, padding=1, padding_mode="reflect")
         self.dropout2 = nn.Dropout(0.2)
-        self.conv3 = nn.Conv2d(4, 8, 2, stride=1, padding=1, padding_mode="reflect")
+        self.conv3 = nn.Conv2d(8, 16, 3, stride=1, padding=1, padding_mode="reflect")
         self.dropout3 = nn.Dropout(0.2)
-        self.conv4 = nn.Conv2d(8, 16, 2, stride=1, padding=1, padding_mode="reflect")
-        self.dropout4 = nn.Dropout(0.2)
-        self.conv5 = nn.Conv2d(16, 32, 2, stride=1, padding=1, padding_mode="reflect")
+        self.conv4 = nn.Conv2d(16, 32, 3, stride=1, padding=1, padding_mode="reflect")
         self.pool = nn.MaxPool2d(2, 2)      # Naudojama sumažinti computational load, bei overfitting. (kernel size, stride). MaxPool naudojamas dažniausiai palyginus su avg ir adptive. 
-        self.fc1 = nn.Linear(32 * 16 * 8 * 4 * 2 * 2, 64)   # Crucial component used to transform the output of a CNN or a RNN into a format that can be used for classification or regression.
-        self.fc2 = nn.Linear(128, len(satelite_classes))
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(4 * 4 * 32, 64)    # Crucial component used to transform the output of a CNN or a RNN into a format that can be used for classification or regression.
+        self.fc2 = nn.Linear(64, len(satelite_classes))
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # Conv1 + ReLU + Pool
+        x = self.dropout1(x)
+        x = self.pool(F.relu(self.conv2(x)))  # Conv2 + ReLU + Pool
+        x = self.dropout2(x)
+        x = self.pool(F.relu(self.conv3(x)))  # Conv3 + ReLU + Pool
+        x = self.dropout3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))  # Fully connected sluoksnis
+        x = self.fc2(x)  # Klasifikavimo sluoksnis
+        return x
+
+model = EurSatCNN()
+print(model)
+
+# # Santrauka su sluoksnių išvesties formomis ir parametrų skaičiumi
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
